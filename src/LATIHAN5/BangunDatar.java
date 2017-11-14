47eee1c9a0ab7d2bc095abf4a148bdcb0c289e83
all <all@Mayendra>
2017-11-15 00:27:05 +0700

    Tugas Latihan UTS5

---
+++ src/LATIHAN5/BangunDatar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LATIHAN5;
+
+/**
+ *
+ * @author all
+ */
+public interface BangunDatar {
+
+    public void hitungLuas();
+
+    public void hitungKeliling();
+}
